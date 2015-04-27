cd53978cdf685e9cab6da9b53c5a2d6954a5ecc6
Tamir Bahar <tamirbah@gitlab.hq>
2015-04-27 13:49:41 +0300

    Fixed the pyside import.

--- DIE.py
+++ DIE.py
@@ -6,7 +6,19 @@ import logging.handlers as handlers
 import os
 from time import ctime
 
-from PySide import QtGui, QtCore
+# This nasty piece of code is here to force the loading of IDA's PySide.
+# Without it, Python attempts to load PySide from the site-packages directory,
+# and failing, as it does not play nicely with IDA.
+import sys
+old_path = sys.path[:]
+try:
+    import idaapi
+    ida_python_path = os.path.dirname(idaapi.__file__)
+    sys.path.insert(0, ida_python_path)
+    from PySide import QtGui, QtCore
+finally:
+    sys.path = old_path
+
 from idaapi import plugin_t
 import idaapi
 import idautils
