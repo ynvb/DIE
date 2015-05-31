0a866069f93cf7d04464cf65ed6273b5b689dc9a
yanivb <ynvbpost@gmail.com>
2015-05-31 18:40:57 +0300

    FIX - fixed CString plugin

--- DIE/Plugins/DataParsers/CStringParser/STDBasicString.py
+++ DIE/Plugins/DataParsers/CStringParser/STDBasicString.py
@@ -2,21 +2,21 @@ from DIE.Lib.DataPluginBase import DataPluginBase
 import idc
 import idaapi
 
-class CStringParser(DataPluginBase):
+class STDBasicString(DataPluginBase):
     """
-    A parser for boolean values
+    A parser for std::basic_string
     """
 
     def __init__(self):
-        super(CStringParser, self).__init__()
-        self.setPluginType("CString")
+        super(STDBasicString, self).__init__()
+        self.setPluginType("std::basic_string")
 
     def registerSupportedTypes(self):
         """
         Register string types
         @return:
         """
-        self.addSuportedType("CSTRING", 0)
+        self.addSuportedType("std::basic_string", 0)
 
     def guessValues(self, rawValue):
         """
@@ -31,8 +31,15 @@ class CStringParser(DataPluginBase):
 
         if value and len(value) >= minLength:
             value, raw_value = self.normalize_raw_value(value)
-            self.addParsedvalue(value, 1, "CString", raw_value)
+            self.addParsedvalue(value, 1, "std::basic_string", raw_value)
             return True
+
+        if not value:
+            value = idc.GetString(rawValue+4, strtype=idc.ASCSTR_C)
+            if value and len(value) >= minLength:
+                value, raw_value = self.normalize_raw_value(value)
+                self.addParsedvalue(value, 1, "std::basic_string", raw_value)
+                return True
 
         return False
 
@@ -57,7 +64,13 @@ class CStringParser(DataPluginBase):
 
         if value:
             value, raw_value = self.normalize_raw_value(value)
-            self.addParsedvalue(value, 1, "CString", raw_value)
+            self.addParsedvalue(value, 1, "std::basic_string", raw_value)
+            return True
+
+        else:
+            value = idc.GetString(rawValue+4, strtype=idc.ASCSTR_C)
+            value, raw_value = self.normalize_raw_value(value)
+            self.addParsedvalue(value, 1, "std::basic_string", raw_value)
             return True
 
         return False
